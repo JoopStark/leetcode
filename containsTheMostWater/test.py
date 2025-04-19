7301f51135704b88417e16099e305879c4fd3f77
Joop <starkjh33@gmail.com>
2025-04-18 22:23:09 -0600

    init regular and water

---
+++ containsTheMostWater/test.py
@@ -0,0 +1,18 @@
+from solution import *
+
+answers = [
+        [[1,8,6,2,5,4,8,3,7], 49],
+        [[1,1], 1],
+        ]
+
+
+
+for i in range(len(answers)):
+    print(f"Case {i}")
+    print("Solution:")
+    print(answer[i][0])
+    print(answer[i][1])
+    print("----------------")
+    print()
+
+
