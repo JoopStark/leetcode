859e1480b29cbd2f98d83cd33ab377059c01203e
Joop <starkjh33@gmail.com>
2025-04-29 20:10:41 -0600

    hash for letter combo made

--- letterComboPN/solution.py
+++ letterComboPN/solution.py
@@ -1,3 +1,14 @@
 def Solution:
     def letterCombinations(self, digits: str) -> list[str]:
-        pass
+        phone_dict = {
+                "2": ["a", "b", "c"],
+                "3": ["d", "e", "g"],
+                "4": ["g", "h", "i"],
+                "5": ["j", "k", "l"],
+                "6": ["m", "n", "o"],
+                "7": ["p", "q", "r", "s"],
+                "8": ["t", "u", "v"],
+                "9": ["w", "x", "y", "z"],
+        }
+
+
